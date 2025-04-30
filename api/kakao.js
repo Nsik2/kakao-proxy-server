6dfe043bef95aec063d63b18731d6553aeaed2ce
Nsik2 <nsik002@gmail.com>
2025-04-30 12:56:22 +0900

    Update kakao.js

--- api/kakao.js
+++ api/kakao.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
         params: {
           w: 'trend_keyword',
           random: 2,
-          n: 100
+          n: 200
         }
       });
 
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
           params: {
             w: 'trend_keyword',
             random: 1,
-            n: 100
+            n: 200
           }
         });
 
